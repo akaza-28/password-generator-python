4dec6db30b670dd01aa66bf26ac74d88229993c1
Hakuji Soyama <98075959+akaza-28@users.noreply.github.com>
2022-09-20 19:42:35 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,5 +1,5 @@
 import random
-
+print("Password Generator by akaza-28")
 password = int(input("Enter the length of your Password : "))
 pas ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-*/@#$%^&()<>?|"
 passw= "".join(random.sample(pas,password ))
