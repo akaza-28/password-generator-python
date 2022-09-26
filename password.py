96e7138acb100aaca7bf0a769765381cdaf665d0
Hakuji Soyama <98075959+akaza-28@users.noreply.github.com>
2022-09-26 19:06:05 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,6 +1,12 @@
 import random
-print("Password Generator by akaza-28")
-password = int(input("Enter the length of your Password : "))
+import bcrypt  
+
+size = int(input("Enter the length of your Password : "))
 pas ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-*/@#$%^&()<>?|"
-passw= "".join(random.sample(pas,password ))
-print(passw)
+passw= "".join(random.sample(pas,size))
+print("Password : ",passw)
+password = str(passw)
+password = password.encode('utf-8')
+hashedPassword = bcrypt.hashpw(password, bcrypt.gensalt())
+
+print("Encrypted : ",hashedPassword)
